f037791fe58142a6a72df858406509aa29310cbb
Mohammad Altif <mohlatif227@gmail.com>
2022-03-20 20:16:54 +0530

    Added Day004 progress: explored local/random resource type

---
+++ Terraform-Day004/variables.tf
@@ -0,0 +1,16 @@
+variable "filename" {
+  default = "/tmp/pets.txt"
+}
+variable "content" {
+  default = "We love pets!"
+}
+variable "prefix" {
+  default = "Mrs"
+}
+
+variable "separator" {
+  default = "."
+}
+variable "length" {
+  default = 2
+}
